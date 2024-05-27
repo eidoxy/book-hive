179250483dc3caab67a7a25ae40ce55099b4890a
Adrian <adriansondang@gmail.com>
2024-05-28 02:30:21 +0700

    build: add CORS for sharing resource on other port 5173

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,20 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import router from './routes';
 
 const app = express();
 
+// Configure CORS
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
